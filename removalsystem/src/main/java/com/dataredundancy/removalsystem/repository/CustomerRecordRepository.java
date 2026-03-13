48bb2aa30613f4687c208029b4e2beeade696ee9
Anj-13 <Anjilag13@gmail.com>
2026-03-13 14:57:25 +0000

    Task 1 - Did the repository check

--- removalsystem/src/main/java/com/dataredundancy/removalsystem/repository/CustomerRecordRepository.java
+++ removalsystem/src/main/java/com/dataredundancy/removalsystem/repository/CustomerRecordRepository.java
@@ -1,5 +1,7 @@
 package com.dataredundancy.removalsystem.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,14 @@ import com.dataredundancy.removalsystem.model.CustomerRecord;
 
 @Repository
 public interface CustomerRecordRepository extends JpaRepository<CustomerRecord, Long> {
-    
+
+	Optional<CustomerRecord> findByFingerPrintHash(String fingerPrintHash);
+
+	boolean existsByFingerPrintHash(String fingerPrintHash);
+
+	boolean existsByEmailIgnoreCase(String email);
+
+	boolean existsByPhone(String phone);
+
+	boolean existsByEmailIgnoreCaseAndPhone(String email, String phone);
 }
