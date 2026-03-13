b895380dc0be5b7728d9b5bfdcfcd45d560fefd2
Anj-13 <Anjilag13@gmail.com>
2026-03-13 13:12:06 +0000

    Task 1 - SpringBoot project created

---
+++ removalsystem/src/main/java/com/dataredundancy/removalsystem/RemovalsystemApplication.java
@@ -0,0 +1,13 @@
+package com.dataredundancy.removalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemovalsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemovalsystemApplication.class, args);
+	}
+
+}
